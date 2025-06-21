8a40e3b76a95f679b91448bb6e47a56ce0415d5a
mkukezhanov <mkukezhanov@naumen.ru>
2025-06-21 16:46:01 +0500

    hw07-data-jpa

---
+++ hw07/src/test/java/ru/otus/hw/services/BookServiceImplIntegrationTest.java
@@ -0,0 +1,57 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.boot.ShellRunnerAutoConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.models.dto.AuthorDto;
+import ru.otus.hw.models.dto.BookDto;
+import ru.otus.hw.models.dto.GenreDto;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        ShellRunnerAutoConfiguration.class
+})
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+class BookServiceImplIntegrationTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private AuthorService authorService;
+
+    @Autowired
+    private GenreService genreService;
+
+
+    @Test
+    void shouldCreateAndFindBook() {
+        AuthorDto author = new AuthorDto(0,"Author_1");
+        author = authorService.save(author);
+        GenreDto genre = genreService.save("Genre_1");
+        BookDto bookDto = new BookDto(0, "new_book", author, List.of(genre));
+        BookDto expectedBook = bookService.create(bookDto);
+
+        Optional<BookDto> actualBookOptional = bookService.findById(expectedBook.id());
+
+        assertThat(actualBookOptional).isPresent().get().isEqualTo(expectedBook);
+
+        var actualBookDto = actualBookOptional.get();
+        assertThatCode(() -> {
+            actualBookDto.title();
+            actualBookDto.author().fullName();
+            actualBookDto.genres().size();
+        }).doesNotThrowAnyException();
+    }
+}
---
+++ hw07/src/test/java/ru/otus/hw/services/CommentServiceImpIntegrationTest.java
@@ -0,0 +1,53 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.boot.ShellRunnerAutoConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.models.dto.AuthorDto;
+import ru.otus.hw.models.dto.BookDto;
+import ru.otus.hw.models.dto.CommentDto;
+import ru.otus.hw.models.dto.GenreDto;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        ShellRunnerAutoConfiguration.class
+})
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+public class CommentServiceImpIntegrationTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private AuthorService authorService;
+
+    @Autowired
+    private GenreService genreService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @Test
+    void shouldSaveAndFindComment() {
+        AuthorDto author = new AuthorDto(0, "Author_1");
+        author = authorService.save(author);
+        GenreDto genre = genreService.save("Genre_1");
+        BookDto bookDto = new BookDto(0, "new_book", author, List.of(genre));
+        BookDto book = bookService.create(bookDto);
+        CommentDto expectedComment = commentService.save(book.id(), "first_comment");
+
+        Optional<CommentDto> actualCommentOptional = commentService.findById(expectedComment.id());
+
+        assertThat(actualCommentOptional).isPresent().get().isEqualTo(expectedComment);
+    }
+}
